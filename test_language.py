7e31ead3739e398c06395a639d7e02f2ce3b4858
custmaster <custmaster@gmail.com>
2021-04-03 21:47:24 +0300

    добавил в репозиторий файлы conftest.py и test_items.py.

---
+++ test_language.py
@@ -0,0 +1,4 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+def test_check_basket_btn(browser):
+    browser.get(link)
+    assert (browser.find_element_by_css_selector(".btn-add-to-basket")) is not None, "Элемент не найден!"
